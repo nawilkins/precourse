979c4e999b01b58b20dabaca81ef6c1c9d1d2923
Newlin Wilkins <nwilkins89@gmail.com>
2014-03-27 18:26:50 -0500

    6th chapter

---
+++ methods.rb
@@ -0,0 +1,124 @@
+# String Methods
+var1 = 'stop'
+var2 = 'deliver rapid desserts'
+var3 = '......TCELES B HSUP  A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+
+puts var1
+puts var2
+puts var3
+
+# puts 'What is your full name?'
+# name =gets.chomp
+# puts 'Did you know there are ' + name.length.to_s + ' characters'
+# puts 'in your name, ' + name
+
+# puts 'What is your first name?'
+# first = gets.chomp
+# puts 'And your middle?'
+# middle = gets.chomp
+# puts 'And your last?'
+# last = gets.chomp
+# full = first + middle + last
+# puts 'Well, there are ' + full.length.to_s + ' letters in your name'
+
+# letters = 'aAbBcCdDeE'
+# puts letters.upcase
+# puts letters.downcase
+# puts letters.swapcase
+# puts letters.capitalize
+# puts ' a'.capitalize
+# puts letters
+
+# line_width = 50
+# puts(				'Old Mother Hubbard'.center(line_width))
+# puts(				'Sat in her cuppord'.center(line_width))
+# puts(				'Eating her curds and whey'.center(line_width))
+# puts(				'When along came a spider'.center(line_width))
+# puts(				'Who sat down beside her'.center(line_width))
+# puts(				'And scared her poor shoe dog away'.center(line_width))
+# line_width = 40
+# str = '--> text <--'
+# puts(str.ljust(line_width))
+# puts(str.center(line_width))
+# puts(str.rjust(line_width))
+# puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
+#Angy Boss
+	# puts 'What do you want?'
+	# question = gets.chomp
+	# response = 'what do you mean "' + question + '"?! you\'re fired!!'
+	# puts response.upcase
+
+#Table of Contents
+	# line_width = 30
+	# difference = 'Getting Started'.length - 'Numbers'.length
+	# puts 'Table of Contents'.center(line_width)
+	# puts 'Chapter 1: Getting Started'.ljust(line_width) + 'page 1'.rjust(line_width - difference)
+	# puts 'Chapter 2: Numbers'.ljust(line_width) + 'page 9'.rjust(line_width - difference)
+	# puts 'Chapter 3: Letters'.ljust(line_width) + 'page 13'.rjust(line_width - difference + 1)
+
+#Math Methods
+	# puts 5**2
+	# puts 5**0.5
+	# puts 7/3
+	# puts 7%3
+	# puts 365%7
+
+	# puts (5-2).abs
+	# puts (2-5).abs
+
+	# puts rand
+	# puts rand
+	# puts rand
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(1))
+	# puts(rand(1))
+	# puts(rand(1))
+	# puts(rand(9999999999999999999))
+	# puts('The weatherman said there is a ')
+	# puts(rand(101).to_s + '% chance of rain')
+	# puts('but you can never trust the weatherman')
+
+	# srand 1976
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts ''
+	# srand 1976
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(100))
+	# puts(rand(100))
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
