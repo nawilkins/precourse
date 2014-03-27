058316a9ca3e2186b72ad15e05d9d95b9c9d4c9b
Newlin Wilkins <nwilkins89@gmail.com>
2014-03-26 21:17:42 -0500

    second file of precourse

---
+++ string.rb
@@ -0,0 +1,21 @@
+puts 'Hello World'
+puts ''
+puts 'Good-bye'
+
+puts 'I like' + 'apple pie'
+puts 'I like' + ' apple pie'
+
+puts 'moo ' * 3
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'you\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
